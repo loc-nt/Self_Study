b2892d68a38aff517860207155e86adb51d7f6e9
loc-nt <48140302+loc-nt@users.noreply.github.com>
2020-11-14 09:21:58 -0800

    exercise

---
+++ lenh.py
@@ -0,0 +1,58 @@
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources/election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+candidate_set = set()
+
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row. 
+    headers = next(file_reader)
+   
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1 
+        
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+
+           # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+           # 2. Begin tracking that candidate's vote count. 
+            candidate_votes[candidate_name] = 0
+
+        candidate_votes[candidate_name] += 1
+
+# Print the candidate vote dictionary.
+print(candidate_votes)
+
+# {'Charles Casper Stockham': 85213, 'Diana DeGette': 272892, 'Raymon Anthony Doane': 11606}
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = int(votes) / int(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate}: received {vote_percentage}% of the vote.") 
+
--- test.py
+++ test.py
@@ -1,8 +1,134 @@
-try:
-    pass
-except expression as identifier:
-    pass
-else:
-    pass
-finally:
-    pass
+print("Hello World")
+type(3)
+type("2019")
+ballots = 1.341
+ballots
+
+
+counties = ["Arapahoe","Denver","Jefferson"]
+
+counties[0]
+counties[-3]
+len(counties)
+counties[0:-2]
+counties[:2]
+counties[1:]
+
+counties.append("El Paso")
+
+counties.insert(2, "El Paso 2")
+
+abc = counties.pop(3)
+abc
+
+counties[2] = "El Paso"
+counties
+
+dcb = counties[2]
+dcb = 3
+dcb
+
+list_a = ["abc", 123, 456]
+list_a
+
+counties_tuple = ("Arapahoe","Denver","Jefferson")
+counties_tuple[1]
+
+
+counties_dict = {}
+counties_dict["Arapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+counties_dict["Jefferson"]
+counties_dict[("Jefferson")]
+
+counties_dict.items()
+a = counties_dict.keys()
+counties_dict.values()
+counties_dict.get("Denver")
+
+a
+counties_dict
+
+
+
+voting_data = []
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+voting_data[0]
+
+
+for county in counties_dict:
+    print(counties_dict[county])
+
+for county, voters in counties_dict.items():    
+    print(f"{county} county has {voters:,} registered voters.")
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+for i in range(len(voting_data)):
+    dict_name = voting_data[i]
+    print(dict_name["county"])
+
+
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+print(message_to_candidate)
+
+
+
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+import csv
+dir(datetime.datetime)
+
+
+dir({'Arapahoe': 422829, 'Denver': 463353, 'Jefferson': 432438})
+dir(str)
+
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
+
+# Open the election results and read the file.
+election_data = open(file_to_load, 'r')
+
+# To do: perform analysis.
+
+# Close the file.
+election_data.close()
+
+
+# Calculation in Python:
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources/election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+
